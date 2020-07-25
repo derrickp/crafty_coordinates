cd3c6311d1b0319bcb8c2b018efcbc9b9cdf3b1e
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-25 13:18:42 -0700

    Remove failing test

--- spec/crafty_coordinates_spec.rb
+++ spec/crafty_coordinates_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe CraftyCoordinates do
   it "has a version number" do
     expect(CraftyCoordinates::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
