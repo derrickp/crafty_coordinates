e95dda355d8ef10cae3e64f5623e4f151b2c3290
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-26 15:34:08 -0700

    Change EVENT_NAME to NAME
    It's just the name for this event. We don't need the added
    EVENT_

--- lib/crafty_coordinates/events/world_created.rb
+++ lib/crafty_coordinates/events/world_created.rb
@@ -6,9 +6,9 @@ require 'crafty_coordinates/minecraft'
 module CraftyCoordinates
   module Events
     class WorldCreated < Event
-      EVENT_NAME = 'world_created'
+      NAME = 'world_created'
 
-      attribute :name, Types::Strict::String.default(EVENT_NAME).constrained(eql: EVENT_NAME)
+      attribute :name, Types::Strict::String.default(NAME).constrained(eql: NAME)
       attribute :world_name, Types::Strict::String
       attribute :created_at, Types::JSON::DateTime
       attribute? :platform, CraftyCoordinates::Minecraft::PLATFORMS
